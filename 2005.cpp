4e55d2e1d285a5afa28a2c01116fdf039610a13b
0uYangXu <37234289+0uYangXu@users.noreply.github.com>
2019-06-20 22:25:26 +0800

    Revert "Update 2005.cpp"
    This reverts commit 75dcc95d57cb3aa253624dbf46a0ac6f07817194.

--- 2005.cpp
+++ 2005.cpp
@@ -1,11 +1,10 @@
-/*2005
-Problem Description
-给定一个日期，输出这个日期是该年的第几天。
-Input
-输入数据有多组，每组占一行，数据格式为YYYY/MM/DD组成，具体参见sample input ,另外，可以向你确保所有的输入数据是合法的。
-Output
-对于每组输入数据，输出一行，表示该日期是该年的第几天。
-*/
+//2005
+//Problem Description
+//给定一个日期，输出这个日期是该年的第几天。
+//Input
+//输入数据有多组，每组占一行，数据格式为YYYY/MM/DD组成，具体参见sample input ,另外，可以向你确保所有的输入数据是合法的。
+//Output
+//对于每组输入数据，输出一行，表示该日期是该年的第几天。
 #include<stdio.h>
 int main()
 {
