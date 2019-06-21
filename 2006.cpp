056b2ac8507fb919bc251b70563f74be15f309a5
0uYangXu <37234289+0uYangXu@users.noreply.github.com>
2019-06-21 22:33:31 +0800

    Update 2006.cpp

--- 2006.cpp
+++ 2006.cpp
@@ -5,3 +5,24 @@
 //输入数据包含多个测试实例，每个测试实例占一行，每行的第一个数为n，表示本组数据一共有n个，接着是n个整数，你可以假设每组数据必定至少存在一个奇数。
 //Output
 //输出每组数中的所有奇数的乘积，对于测试实例，输出一行。
+#include<stdio.h>
+int main()
+{
+    int product;
+    int n,tmp;
+    while(scanf("%d",&n)==1)
+    {
+    product=1;
+    while(n-->0)
+    {
+        scanf("%d",&tmp);
+        if(tmp%2==1)
+        {
+            product*=tmp;
+        }
+    }
+    printf("%d\n",product);
+    }
+    return 0;
+}
+
