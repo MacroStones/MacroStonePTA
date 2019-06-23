5fbbefa80e34ac838fccfef7e10eb6d649077f3e
0uYangXu <37234289+0uYangXu@users.noreply.github.com>
2019-06-23 20:24:21 +0800

    Update 2010.cpp

--- 2010.cpp
+++ 2010.cpp
@@ -8,3 +8,33 @@
 //对于每个测试实例，要求输出所有在给定范围内的水仙花数，就是说，输出的水仙花数必须大于等于m,并且小于等于n，如果有多个，则要求从小到大排列在一行内输出，之间用一个空格隔开;
 //如果给定的范围内不存在水仙花数，则输出no;
 //每个测试实例的输出占一行。
+#include<stdio.h>
+int main()
+{
+	int m,n;
+	bool no,first;
+	while(scanf("%d%d",&m,&n))
+	{
+		no=true;first=true;
+		for(int i=m;i<=n;i++)
+		{
+			if(i==(i/100)*(i/100)*(i/100)+\
+                  ((i/10)%10)*((i/10)%10)*((i/10)%10)+\
+                  (i%10)*(i%10)*(i%10))
+			{
+				if(first)
+				{
+					printf("%d",i);
+					first=false;
+					no=false;
+				}
+				else
+					printf(" %d",i);
+			}
+		}
+		if(no)
+			printf("no");
+		printf("\n");
+	}
+	return 0;
+}
