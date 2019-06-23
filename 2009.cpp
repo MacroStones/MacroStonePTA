ffaf9c704494f507e57d45a5cdfe6ccf49175d80
0uYangXu <37234289+0uYangXu@users.noreply.github.com>
2019-06-23 10:14:21 +0800

    Update 2009.cpp

--- 2009.cpp
+++ 2009.cpp
@@ -5,4 +5,21 @@
 //输入数据有多组，每组占一行，由两个整数n（n<10000）和m(m<1000)组成，n和m的含义如前所述。
 //Output
 //对于每组输入数据，输出该数列的和，每个测试实例占一行，要求精度保留2位小数。
-
+#include<stdio.h>
+#include<math.h>
+int main()
+{
+	int n,m;
+	double sum,last;
+	while(scanf("%d %d",&n,&m)==2)
+	{
+		sum=last=n;
+		for(int i=1;i<m;i++)
+		{
+			last=sqrt(last);
+			sum+=last;
+		}
+		printf("%.2f\n",sum);
+	}
+	return 0;
+}
